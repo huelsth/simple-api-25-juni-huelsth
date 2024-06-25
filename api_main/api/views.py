3c9c0ca08b867f0bd17cdfc0e188171a77dbca8e
huelsthe <thomas.huelsken@dci-student.org>
2024-06-25 14:26:24 +0200

    fix:addedmore

--- api_main/api/views.py
+++ api_main/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from ninja import NinjaAPI
 
-# Create your views here.
+api = NinjaAPI()
+
+@api.get("/hello")
+def hello(request):
+    return {"message": "Hello World"}
+
+# myproject/urls.py
+from django.contrib import admin
+from django.urls import path
+from api.views import api
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', api.urls),
+]
